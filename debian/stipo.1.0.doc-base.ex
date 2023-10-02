77e3222ccc67b649a8641518b7da4f3e820e418a
Doston <hunter@doston.net>
2023-10-02 06:51:32 +0500

    Packaged up

---
+++ debian/stipo.1.0.doc-base.ex
@@ -0,0 +1,20 @@
+Document: stipo.1.0
+Title: Debian stipo.1.0 Manual
+Author: <insert document author here>
+Abstract: This manual describes what stipo.1.0 is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/stipo.1.0/stipo.1.0.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/stipo.1.0/stipo.1.0.ps.gz
+
+Format: text
+Files: /usr/share/doc/stipo.1.0/stipo.1.0.text.gz
+
+Format: HTML
+Index: /usr/share/doc/stipo.1.0/html/index.html
+Files: /usr/share/doc/stipo.1.0/html/*.html
